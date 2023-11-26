e792f05f808ca55777933207252da94135b94714
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2023-11-25 21:58:57 -0300

    Add guesthouse details
    Guesthouse details are loaded when the button with the warehouse name is clicked

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
+      id: '',
+      brand_name: '',
+      description: '',
+      phone_number: '',
+      email: '',
+      pet_policy: '',
+      guesthouse_policy: '',
+      checkin_time: '',
+      checkout_time: '',
+      payment_methods: '',
+      address: '',
+      average_rating: '',
+
       guesthouses: []
     }
   },
@@ -17,7 +30,40 @@ const app = Vue.createApp({
 
       data.forEach(guesthouse => this.guesthouses.push(guesthouse));
 
-      console.log(this.guesthouses)
+      console.log(this.guesthouses);
+    },
+    
+    async loadDetails(id) {
+      let response = await fetch(`http://localhost:3000/api/v1/guesthouses/${id}`)
+
+      let guesthouse = await response.json();
+      
+      this.id = guesthouse.id;
+      this.brand_name = guesthouse.brand_name;
+      this.average_rating = guesthouse.average_rating;
+      this.description = guesthouse.description;
+      this.phone_number = guesthouse.phone_number;
+      this.email = guesthouse.email;
+      this.pet_policy = guesthouse.pet_policy;
+      this.guesthouse_policy = guesthouse.guesthouse_policy;
+      this.checkin_time = guesthouse.checkin_time;
+      this.checkout_time = guesthouse.checkout_time;
+      this.payment_methods = [
+        guesthouse.payment_method_one,
+        guesthouse.payment_method_two,
+        guesthouse.payment_method_three
+      ].filter(pm => pm !== '').join(' | ');
+      this.address = [
+        guesthouse.address.street_name,
+        guesthouse.address.number,
+        guesthouse.address.complement,
+        guesthouse.address.neighbourhood,
+        guesthouse.address.postal_code,
+        guesthouse.address.city,
+        guesthouse.address.state
+      ].filter(info => info !== '').join(', ');
+
+      document.querySelector('.guesthouse-details').removeAttribute('hidden')
     }
   }
 })
