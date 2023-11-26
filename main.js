b89878beb51b90a3f514840950b39a7c0ce67244
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2023-11-26 20:55:14 -0300

    Add exception handling

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       searchText: '',
+      errorMessage: '',
 
       id: '',
       brandName: '',
@@ -21,8 +22,6 @@ const app = Vue.createApp({
       
       hideGuesthouseDetails: true,
       hideRooms: true,
-
-      roomResponse: ''
     }
   },
 
@@ -33,27 +32,35 @@ const app = Vue.createApp({
   methods: {
     async getGuesthouses() {
       this.hideDetails();
+      this.errorMessage = '';
 
-      let url = '';
-      if (this.searchText) {
-        url = `http://localhost:3000/api/v1/guesthouses/?search=${this.searchText}`;
-      } else {
-        url = `http://localhost:3000/api/v1/guesthouses`;
+      try {
+        let url = '';
+        if (this.searchText) {
+          url = `http://localhost:3000/api/v1/guesthouses/?search=${this.searchText}`;
+        } else {
+          url = `http://localhost:3000/api/v1/guesthouses`;
+        }
+
+        let response = await fetch(url);
+
+        if (response.status !== 200) {
+          this.errorMessage = 'Dados indisponívei';
+        }
+
+        let data = await response.json();
+        this.guesthouses = [];
+  
+        data.forEach(item => {
+          let guesthouse = new Object();
+          guesthouse.id = item.id;
+          guesthouse.brandName = item.brand_name;
+          this.guesthouses.push(guesthouse)
+        });
+        document.querySelector('main').hidden = false;
+      } catch(e) {
+        this.errorMessage = 'Dados indisponíveis';
       }
-
-      let response = await fetch(url);
-      let data = await response.json();
-
-      this.guesthouses = [];
-
-      data.forEach(item => {
-        let guesthouse = new Object();
-        guesthouse.id = item.id;
-        guesthouse.brandName = item.brand_name;
-        this.guesthouses.push(guesthouse)
-      });
-
-      document.querySelector('main').hidden = false;
     },
 
     loadAll() {
@@ -64,66 +71,87 @@ const app = Vue.createApp({
     async getDetails(id) {
       this.hideRoomForm = true;
       this.rooms = [];
+      this.errorMessage = '';
 
-      let response = await fetch(`http://localhost:3000/api/v1/guesthouses/${id}`)
-      let guesthouse = await response.json();
-      
-      this.id = guesthouse.id;
-      this.brandName = guesthouse.brand_name;
-      this.averageRating = guesthouse.average_rating;
-      this.description = guesthouse.description;
-      this.phoneNumber = guesthouse.phone_number;
-      this.email = guesthouse.email;
-      this.petPolicy = guesthouse.pet_policy;
-      this.guesthousePolicy = guesthouse.guesthouse_policy;
-      this.checkinTime = guesthouse.checkin_time;
-      this.checkoutTime = guesthouse.checkout_time;
-      this.paymentMethods = [
-        guesthouse.payment_method_one,
-        guesthouse.payment_method_two,
-        guesthouse.payment_method_three
-      ].filter(pm => pm !== '').join(' | ');
-      this.address = [
-        guesthouse.address.street_name,
-        guesthouse.address.number,
-        guesthouse.address.complement,
-        guesthouse.address.neighbourhood,
-        guesthouse.address.postal_code,
-        guesthouse.address.city,
-        guesthouse.address.state
-      ].filter(info => info !== '').join(', ');
+      try {
+        let response = await fetch(`http://localhost:3000/api/v1/guesthouses/${id}`)
 
-      this.hideGuesthouseDetails = false;
+        if (response.status !== 200) {
+          this.errorMessage = 'Dados indisponívei';
+        }
+
+        let guesthouse = await response.json();
+        
+        this.id = guesthouse.id;
+        this.brandName = guesthouse.brand_name;
+        this.averageRating = guesthouse.average_rating;
+        this.description = guesthouse.description;
+        this.phoneNumber = guesthouse.phone_number;
+        this.email = guesthouse.email;
+        this.petPolicy = guesthouse.pet_policy;
+        this.guesthousePolicy = guesthouse.guesthouse_policy;
+        this.checkinTime = guesthouse.checkin_time;
+        this.checkoutTime = guesthouse.checkout_time;
+        this.paymentMethods = [
+          guesthouse.payment_method_one,
+          guesthouse.payment_method_two,
+          guesthouse.payment_method_three
+        ].filter(pm => pm !== '').join(' | ');
+        this.address = [
+          guesthouse.address.street_name,
+          guesthouse.address.number,
+          guesthouse.address.complement,
+          guesthouse.address.neighbourhood,
+          guesthouse.address.postal_code,
+          guesthouse.address.city,
+          guesthouse.address.state
+        ].filter(info => info !== '').join(', ');
+  
+        this.hideGuesthouseDetails = false;
+      } catch (e) {
+        this.errorMessage = 'Dados indisponíveis';
+      }
     },
 
     async getRooms(id) {
-      let response = await fetch(`http://localhost:3000/api/v1/guesthouses/${id}/rooms`)
-      let rooms = await response.json();
+      this.errorMessage = '';
 
-      this.rooms = [];
+      try {
+        let response = await fetch(`http://localhost:3000/api/v1/guesthouses/${id}/rooms`)
 
-      rooms.forEach(item => {
-        let room = new Object();
-        room.id = item.id;
-        room.name = item.name;
-        room.description = item.description;
-        room.dailyRate = this.convertToCurrency(item.daily_rate);
-        room.dimension = item.dimension;
-        room.maxPeople = item.max_people;
-        room.features = [
-          item.private_bathroom ? 'Banheiro próprio' : '',
-          item.balcony ? 'Varanda' : '',
-          item.air_conditioning ? 'Ar-condicionado' : '',
-          item.tv ? 'TV': '',
-          item.closet ? 'Guarda-roupas' : '',
-          item.safe ? 'Cofre' : '',
-          item.accessibility ? 'Acessível para pessoas com deficiência' : ''
-        ].filter(feat => feat !== '');
+        if (response.status !== 200) {
+          this.errorMessage = 'Dados indisponívei';
+        }
 
-        this.rooms.push(room);
-      });
-
-      this.hideRooms = false;
+        let rooms = await response.json();
+  
+        this.rooms = [];
+  
+        rooms.forEach(item => {
+          let room = new Object();
+          room.id = item.id;
+          room.name = item.name;
+          room.description = item.description;
+          room.dailyRate = this.convertToCurrency(item.daily_rate);
+          room.dimension = item.dimension;
+          room.maxPeople = item.max_people;
+          room.features = [
+            item.private_bathroom ? 'Banheiro próprio' : '',
+            item.balcony ? 'Varanda' : '',
+            item.air_conditioning ? 'Ar-condicionado' : '',
+            item.tv ? 'TV': '',
+            item.closet ? 'Guarda-roupas' : '',
+            item.safe ? 'Cofre' : '',
+            item.accessibility ? 'Acessível para pessoas com deficiência' : ''
+          ].filter(feat => feat !== '');
+  
+          this.rooms.push(room);
+        });
+  
+        this.hideRooms = false;
+      } catch (e) {
+        this.errorMessage = 'Dados indisponíveis'
+      }
     },
 
     hideDetails() {
@@ -132,25 +160,36 @@ const app = Vue.createApp({
     },
 
     async checkAvailability(roomId) {
-      let room = this.rooms.find(r => r.id === roomId);
+      this.errorMessage = '';
+      
+      try {
+        let room = this.rooms.find(r => r.id === roomId);
+  
+        let checkinValue = document.getElementById(`checkin-${roomId}`).value;
+        let checkoutValue = document.getElementById(`checkout-${roomId}`).value;
+        let guestCountValue = document.getElementById(`guest-count-${roomId}`).value;
+  
+        let url = `http://localhost:3000/api/v1/rooms/${roomId}/check_availability/?` +
+                  `checkin=${checkinValue}&checkout=${checkoutValue}&guest_count=${guestCountValue}`
+  
+        let response = await fetch(url)
 
-      let checkinValue = document.getElementById(`checkin-${roomId}`).value;
-      let checkoutValue = document.getElementById(`checkout-${roomId}`).value;
-      let guestCountValue = document.getElementById(`guest-count-${roomId}`).value;
+        if (response.status !== 200) {
+          this.errorMessage = 'Dados indisponívei';
+        }
 
-      let url = `http://localhost:3000/api/v1/rooms/${roomId}/check_availability/?` +
-                `checkin=${checkinValue}&checkout=${checkoutValue}&guest_count=${guestCountValue}`
-
-      let response = await fetch(url)      
-      let responseJson = await response.json();
-      if (responseJson.stay_total) {
-        let total = this.convertToCurrency(responseJson.stay_total);
-
-        room.response = `Total da hospedagem: ${total}`
-      } else if (responseJson.error === 'Quarto não disponível no período informado'){
-        room.response = responseJson.error;
-      } else {
-        room.response = "Dados inválidos";
+        let responseJson = await response.json();
+        if (responseJson.stay_total) {
+          let total = this.convertToCurrency(responseJson.stay_total);
+  
+          room.response = `Total da hospedagem: ${total}`
+        } else if (responseJson.error === 'Quarto não disponível no período informado'){
+          room.response = responseJson.error;
+        } else {
+          room.response = "Dados inválidos";
+        }
+      } catch (e) {
+        this.errorMessage = 'Dados indisponíveis';
       }
     },
 
